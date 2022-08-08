6456d484bfcaa6e7410cfe3eb6be1442f096c3a7
Dmitry <d1d1d1@inbox.ru>
2022-08-09 02:37:33 +0300

    update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -520,7 +520,7 @@ class MainController extends Controller
 		$this->request->session()->put('cityId', $city->id);
 		$this->request->session()->put('cityAlias', $city->alias);
 		$this->request->session()->put('cityName', $city->name);
-		$this->request->session()->put('cityName', $city->phone ? $city->phoneFormatted() : '');
+		$this->request->session()->put('cityPhone', $city->phone ? $city->phoneFormatted() : '');
 		
 		return response()->json(['status' => 'success', 'cityAlias' => $city->alias]);
 	}
