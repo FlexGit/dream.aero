34955f404c93c8f1a335aaa5171775ab0d39b9be
Dmitry <d1d1d1@inbox.ru>
2022-08-09 15:18:48 +0300

    update

--- resources/views/admin/position/modal/product/edit.blade.php
+++ resources/views/admin/position/modal/product/edit.blade.php
@@ -4,44 +4,9 @@
 <input type="hidden" id="flight_simulator_id" name="flight_simulator_id" value="{{ $position->flight_simulator_id }}">
 
 <div class="row">
-	<div class="col">
-		<div class="form-group">
-			<label for="city_id">Город</label>
-			<select class="form-control" id="city_id" name="city_id">
-				<option value=""></option>
-				@foreach($cities ?? [] as $city)
-					<option value="{{ $city->id }}" @if($city->id == $position->city_id) selected @endif>{{ $city->name }}</option>
-				@endforeach
-			</select>
-		</div>
-	</div>
-	<div class="col">
-		<div class="form-group">
-			<label for="promo_id">Акция</label>
-			<select class="form-control" id="promo_id" name="promo_id" disabled>
-				<option value=""></option>
-				@foreach($promos ?? [] as $promo)
-					<option value="{{ $promo->id }}" @if($promo->id == $position->promo_id) selected @endif>{{ $promo->valueFormatted() }}</option>
-				@endforeach
-			</select>
-		</div>
-	</div>
-	<div class="col">
-		<div class="form-group">
-			<label for="promocode_id">Промокод</label>
-			<select class="form-control" id="promocode_id" name="promocode_id" disabled>
-				<option value=""></option>
-				@foreach($promocodes ?? [] as $promocode)
-					<option value="{{ $promocode->id }}" @if($promocode->id == $position->promocode_id) selected @endif>{{ $promocode->valueFormatted() }}</option>
-				@endforeach
-			</select>
-		</div>
-	</div>
-</div>
-<div class="row">
 	<div class="col-4">
 		<div class="form-group">
-			<label for="product_id">Продукт</label>
+			<label for="product_id">Product</label>
 			<select class="form-control js-product" id="product_id" name="product_id">
 				<option></option>
 				@foreach($products ?? [] as $productTypeName => $productId)
@@ -57,15 +22,15 @@
 </div>
 <div class="row">
 	<div class="col-8">
-		<label for="comment">Комментарий</label>
+		<label for="comment">Comment</label>
 		<textarea class="form-control" id="comment" name="comment" rows="2">{{ isset($position->data_json['comment']) ? $position->data_json['comment'] : '' }}</textarea>
 	</div>
 	<div class="col-4 text-right">
 		<div class="form-group mt-4">
-			<div class="custom-control custom-switch custom-control-inline">
+			{{--<div class="custom-control custom-switch custom-control-inline">
 				<input type="checkbox" id="is_free" name="is_free" value="1" @if(!$position->amount) checked @endif class="custom-control-input">
-				<label class="custom-control-label font-weight-normal" for="is_free">Бесплатно</label>
-			</div>
+				<label class="custom-control-label font-weight-normal" for="is_free">Free</label>
+			</div>--}}
 			<div id="amount-text">
 				<h1 class="d-inline-block">{{ $position->amount }}</h1> <i class="fas fa-ruble-sign" style="font-size: 25px;"></i>
 			</div>
