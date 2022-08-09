9ed59a675c7d78251e430b35bd19680548cdfe3f
Dmitry <d1d1d1@inbox.ru>
2022-08-09 12:54:01 +0300

    update

--- app/Jobs/SendCertificateEmail.php
+++ app/Jobs/SendCertificateEmail.php
@@ -80,7 +80,6 @@ class SendCertificateEmail extends Job implements ShouldQueue {
 
 		$subject = env('APP_NAME') . ': Flight Voucher';
 		
-		\Log::debug(222);
 		Mail::send(['html' => "admin.emails.send_certificate"], $messageData, function ($message) use ($subject, $recipients, $certificateRulesFileName, $bcc, $certificateFilePath) {
 			/** @var \Illuminate\Mail\Message $message */
 			$message->subject($subject);
