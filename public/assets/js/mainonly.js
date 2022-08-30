e1097f73f5ed82dbb65cc1f669b7de8afad9149e
Dmitry <d1d1d1@inbox.ru>
2022-08-30 12:41:31 +0300

    small fixes

--- public/assets/js/mainonly.js
+++ public/assets/js/mainonly.js
@@ -3,26 +3,6 @@ $(function(){
     $('#varsL img').height($('#varsR').height());
     $teamer = 0;
 
-    /*$('.popup-mainpay input').click(function() {
-        if (this.id == 'clemail'){
-            $(this).css("margin-bottom","0px");
-            $('.popup-mainpay #podskazka').show();
-        } else if($(this).attr('type') != 'checkbox' && this.id != 'productPromoCodeId') {
-            $(this).css("margin-bottom","24px");
-            $('.popup-mainpay #podskazka').hide();
-        }
-    });*/
-
-    /*$('#main-bronsert #rtitle').change(function() {
-        var txt4price = getbronprice();
-        if (txt4price[1]) {
-            document.getElementById('price-popup').innerHTML = txt4price[1];
-        }
-        $('#pop-price').val(txt4price[1].replace("Стоимость",""));
-        $('#type-time').val($(this).val() + ' мин');
-        //promoprice('bron');
-    });*/
-
     $(".team .owl-carousel").owlCarousel({
         items:4,
         margin:50,
@@ -62,67 +42,4 @@ $(function(){
     $('.reviews-next').click(function(){
         reviewsCarousel.trigger('next.owl.carousel');
     });
-
-    /*$('.form_open').click(function() {
-        $.ajax({
-            url: '/admin/dealajax',
-            type: 'POST',
-            dataType: 'json',
-            data:{
-                'action': 'openform',
-                'formname': $(this).attr('data-formname'),
-                'selectcity': $("#current_city").text()
-            },
-            success: function(data) {
-                $('#editform').html(data);
-            }
-        });
-    });*/
-
-    /*$('body').on('change',' #tarif_time', function() {
-        getbrontarif();
-    });*/
-
-    /*$('body').on('change',' #selectproduct', function() {
-        selected = $(this).find('option:selected');
-        $('#tarif_type').val(selected.attr("data-type"));
-        $('#tarif').val(selected.val());
-        TotalPrice();
-    });*/
-
-    /*$('#deliverych').change(function(){
-        if ($(this).is(":checked")) {
-            $('#deliveryinput').show();
-            $("#deliadd").prop('required',true);
-            if (document.getElementById('delcity').value == 'Санкт-Петербург') {
-                document.getElementById('deliaddtxt').innerHTML = 'Только в пределах КАД<br/>Доставка осуществляется в течение трех дней';
-            } else if (document.getElementById('delcity').value=='Москва') {
-                document.getElementById('deliaddtxt').innerHTML = 'Только в пределах МКАД<br/>Доставка осуществляется в течение трех дней';
-            } else {
-                document.getElementById('deliaddtxt').innerHTML = 'в пределах города<br/>Доставка осуществляется в течение трех дней';
-            }
-        } else {
-            $('#deliveryinput').hide();
-            $("#deliadd").prop('required',false);
-            document.getElementById('deliaddtxt').innerHTML = '';
-            $("#deliadd").val('');
-        }
-    });*/
 });
-
-/*function mainbron(type) {
-    if (type == 'paytab') {
-        $('#main-bronsert #sernum').attr('required',true);
-        $('#main-bronsert #sernum').show();
-        $('#main-bronsert #on-price-popup').hide();
-        $('#main-bronsert #aktext').hide();
-        stitle = $('#main-bronsert #serttitle').val().replace("Бронирование полета","Запись по сертификату");$('#main-bronsert #serttitle').val(stitle);
-    } else {
-        $('#main-bronsert #sernum').val('');
-        $('#main-bronsert #sernum').attr('required',false);
-        $('#main-bronsert #sernum').hide();
-        $('#main-bronsert #on-price-popup').show();
-        $('#main-bronsert #aktext').show();
-        stitle = $('#main-bronsert #serttitle').val().replace("Запись по сертификату","Бронирование полета");$('#main-bronsert #serttitle').val(stitle);
-    }
-}*/
