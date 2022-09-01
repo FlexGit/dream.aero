d81acfdbcf0f2c1da4862a7320861100f989c24e
Dmitry <d1d1d1@inbox.ru>
2022-09-01 14:24:05 +0300

    update

--- resources/views/includes/promobox.blade.php
+++ resources/views/includes/promobox.blade.php
@@ -2,9 +2,9 @@
 	<div class="popup popup-promo">
 		<a class="close" href="javascript:void(0)" onclick="localStorage.setItem('promobox-{{ $promobox ? $promobox->id : '' }}', true);">×</a>
 		<div class="content">
-			<h2>{!! $promobox->title !!}</h2>
+			<h2>{!! $promobox ? $promobox->title : '' !!}</h2>
 			<div>
-				{!! $promobox->preview_text ? $promobox->preview_text : '' !!}
+				{!! ($promobox && $promobox->preview_text) ? $promobox->preview_text : '' !!}
 			</div>
 		</div>
 	</div>
