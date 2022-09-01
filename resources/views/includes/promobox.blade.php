6b785786cfb02fe1b023bd98e43e02a3ed2747fa
Dmitry <d1d1d1@inbox.ru>
2022-09-01 14:23:07 +0300

    update

--- resources/views/includes/promobox.blade.php
+++ resources/views/includes/promobox.blade.php
@@ -1,4 +1,4 @@
-<div id="promobox" class="overlay @if(!$promobox) hidden @endif" data-alias="{{ $promobox->id }}">
+<div id="promobox" class="overlay @if(!$promobox) hidden @endif" data-alias="{{ $promobox ? $promobox->id : '' }}">
 	<div class="popup popup-promo">
 		<a class="close" href="javascript:void(0)" onclick="localStorage.setItem('promobox-{{ $promobox->id }}', true);">×</a>
 		<div class="content">
