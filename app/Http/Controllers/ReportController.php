50c0ecb2fc6cdd40fd2168ccbbd1c4b0b4991622
Dmitry <d1d1d1@inbox.ru>
2022-10-03 16:33:49 +0300

    update

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -673,9 +673,9 @@ class ReportController extends Controller {
 				->whereRelation('status', 'statuses.alias', '=', Bill::PAYED_STATUS)
 				->whereRelation('paymentMethod', 'payment_methods.alias', '=', $paymentMethodAlias)
 				->whereHas('deal', function ($query) use ($city, $location) {
-					return $query->whereRelation('status', 'statuses.alias', '=', Deal::CONFIRMED_STATUS)
-						->where('city_id', $city->id)
+					$query->where('city_id', $city->id)
 						->where('location', $location->id);
+					return $query->whereRelation('status', 'statuses.alias', '=', Deal::CONFIRMED_STATUS);
 				});
 			if ($operationType) {
 				if ($productIds) {
