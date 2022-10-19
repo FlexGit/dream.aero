f11622eed9a22251eab6c9af5ec0befa0c01b115
Dmitry <d1d1d1@inbox.ru>
2022-10-19 20:39:36 +0300

    update

--- resources/views/dream-aero/includes/js.blade.php
+++ resources/views/dream-aero/includes/js.blade.php
@@ -6,21 +6,14 @@
 		s.parentNode.insertBefore(ta, s);
 	})();
 </script>
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-952284596"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11002510393"></script>
 <script>
-	function gtag_report_conversion(url) {
-		var callback = function () {
-			if (typeof(url) != 'undefined') {
-				window.location = url;
-			}
-		};
-		gtag('event', 'conversion', {
-			'send_to': 'AW-952284596/BsITCMeOr5EBELTrisYD',
-			'transaction_id': '',
-			'event_callback': callback
-		});
-		return false;
-	}
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'AW-11002510393');
 </script>
 <!-- Facebook Pixel Code -->
 <script>
