b208138a7f6c159063f4cda32f67ae87fbc08920
Dmitry <d1d1d1@inbox.ru>
2022-11-21 18:04:24 +0300

    update

--- resources/views/admin/report/cash-flow/balance-export.blade.php
+++ resources/views/admin/report/cash-flow/balance-export.blade.php
@@ -2,17 +2,17 @@
 	<tbody>
 	<tr>
 		<td></td>
-		<td style="text-align: center;font-weight: bold;">{{ \Carbon\Carbon::parse($dateFromAtTimestamp)->format('m/d/Y') }}</td>
-		<td style="text-align: center;font-weight: bold;">{{ \Carbon\Carbon::parse($dateToAtTimestamp)->format('m/d/Y') }}</td>
+		<td style="text-align: center;font-weight: bold;">{{ \Carbon\Carbon::parse($dateFromAt)->format('m/d/Y g:i A') }}</td>
+		<td style="text-align: center;font-weight: bold;">{{ \Carbon\Carbon::parse($dateToAt)->format('m/d/Y g:i A') }}</td>
 	</tr>
 	@foreach($paymentMethods as $paymentMethod)
-		@if(!isset($balanceItems[$dateFromAtTimestamp][$paymentMethod->alias]))
+		@if(!isset($balanceItems[$dateFromAt][$paymentMethod->alias]))
 			@continue
 		@endif
 		<tr>
 			<td style="text-align: center;">{{ $paymentMethod->name }}</td>
-			<td style="text-align: right;">{{ number_format($balanceItems[$dateFromAtTimestamp][$paymentMethod->alias], 2, '.', '') }}</td>
-			<td style="text-align: right;">{{ number_format($balanceItems[$dateToAtTimestamp][$paymentMethod->alias], 2, '.', '') }}</td>
+			<td style="text-align: right;">{{ number_format($balanceItems[$dateFromAt][$paymentMethod->alias], 2, '.', '') }}</td>
+			<td style="text-align: right;">{{ number_format($balanceItems[$dateToAt][$paymentMethod->alias], 2, '.', '') }}</td>
 		</tr>
 	@endforeach
 	</tbody>
